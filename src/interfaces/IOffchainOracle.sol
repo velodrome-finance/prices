68f33dcd4d0ee73256ed43916191c08fa636fe01
imollov <ivomollov@gmail.com>
2025-05-07 10:37:36 +0300

    fix: add missing newline for code readability in IOffchainOracle interface

--- src/interfaces/IOffchainOracle.sol
+++ src/interfaces/IOffchainOracle.sol
@@ -6,5 +6,6 @@ import "./IERC20.sol";
 
 interface IOffchainOracle {
     function getRateWithCustomConnectors(IERC20 srcToken, IERC20 dstToken, bool useWrappers, IERC20[] memory customConnectors, uint256 thresholdFilter) external view returns (uint256 weightedRate);
+
     function getManyRatesWithCustomConnectors(IERC20[] calldata srcTokens, IERC20 dstToken, bool useWrappers, IERC20[] calldata customConnectors, uint256 thresholdFilter) external view returns (uint256[] memory weightedRates);
 }
