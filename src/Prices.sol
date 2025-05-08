a27103681d84f17c51f88a1b203d04b4ac8a82d4
Ivelin Mollov <ivomollov@gmail.com>
2025-05-08 10:22:42 +0300

    feat: add fetch price and store price functions (#7)
    * feat: add fetchPrice and storePrice functions
    
    * refactor: simplify storePrice function parameter and logic
    
    * fix: simplify storePrices function parameters
    
    * fix: add missing newline for code readability in IOffchainOracle interface
    
    * refactor: make onlyOwner and onlyOwnerOrKeeper modifiers
    
    * perf: cache timeWindow before read

--- src/Prices.sol
+++ src/Prices.sol
@@ -38,19 +38,29 @@ contract Prices {
         thresholdFilter = _thresholdFilter;
     }
 
+    /// @notice Modifier that ensures the caller is an owner.
+    modifier onlyOwner() {
+        require(owners[msg.sender]);
+        _;
+    }
+
+    /// @notice Modifier that ensures the caller is an owner or a keeper.
+    modifier onlyOwnerOrKeeper() {
+        require(owners[msg.sender] || keepers[msg.sender]);
+        _;
+    }
+
     /// @notice Adds an owner to the owners set.
     /// @dev Can only be called by an owner.
     /// @param _owner The address to be added.
-    function addOwner(address _owner) public {
-        _onlyOwner();
+    function addOwner(address _owner) public onlyOwner {
         owners[_owner] = true;
     }
 
     /// @notice Removes an owner from the owners set.
     /// @dev Can only be called by an owner.
     /// @param _owner The address to be added.
-    function removeOwner(address _owner) public {
-        _onlyOwner();
+    function removeOwner(address _owner) public onlyOwner {
         require(msg.sender != _owner, "Owner cannot remove themself");
         owners[_owner] = false;
     }
@@ -58,32 +68,28 @@ contract Prices {
     /// @notice Adds a keeper to the keepers set.
     /// @dev Can only be called by an owner.
     /// @param _keeper The address to be added.
-    function addKeeper(address _keeper) public {
-        _onlyOwner();
+    function addKeeper(address _keeper) public onlyOwner {
         keepers[_keeper] = true;
     }
 
     /// @notice Removes a keeper from the keepers set.
     /// @dev Can only be called by an owner.
     /// @param _keeper The address to be removed.
-    function removeKeeper(address _keeper) public {
-        _onlyOwner();
+    function removeKeeper(address _keeper) public onlyOwner {
         keepers[_keeper] = false;
     }
 
     /// @notice Sets a new oracle.
     /// @dev Can only be called by an owner.
     /// @param _oracle The address of the new oracle.
-    function setOracle(IOffchainOracle _oracle) public {
-        _onlyOwner();
+    function setOracle(IOffchainOracle _oracle) public onlyOwner {
         oracle = _oracle;
     }
 
     /// @notice Adds a connector token for pricing.
     /// @dev Can only be called by an owner.
     /// @param _token The token to be added.
-    function addConnector(address _token) public {
-        _onlyOwner();
+    function addConnector(address _token) public onlyOwner {
         bool isConnector = false;
         for (uint i = 0; i < connectors.length; i++) {
             if (address(connectors[i]) == _token) {
@@ -100,8 +106,7 @@ contract Prices {
     /// @notice Removes a connector token.
     /// @dev Can only be called by an owner.
     /// @param _token The token to be removed.
-    function removeConnector(address _token) public {
-        _onlyOwner();
+    function removeConnector(address _token) public onlyOwner {
         for (uint i = 0; i < connectors.length; i++) {
             if (address(connectors[i]) == _token) {
                 connectors[i] = connectors[connectors.length - 1];
@@ -114,33 +119,48 @@ contract Prices {
     /// @notice Sets a new stable token to price against.
     /// @dev Can only be called by an owner.
     /// @param _stableToken The address of the new stable token.
-    function setStableToken(address _stableToken) public {
-        _onlyOwner();
+    function setStableToken(address _stableToken) public onlyOwner {
         stableToken = _stableToken;
     }
 
     /// @notice Sets a new threshold filter for pricing.
     /// @dev Can only be called by an owner.
     /// @param _thresholdFilter The new threshold filter.
-    function setThresholdFilter(uint256 _thresholdFilter) public {
-        _onlyOwner();
+    function setThresholdFilter(uint256 _thresholdFilter) public onlyOwner {
         thresholdFilter = _thresholdFilter;
     }
 
     /// @notice Sets a new time window for posting prices.
     /// @dev Can only be called by an owner.
     /// @param _timeWindow The new time window.
-    function setTimeWindow(uint256 _timeWindow) public {
-        _onlyOwner();
+    function setTimeWindow(uint256 _timeWindow) public onlyOwner {
         timeWindow = _timeWindow;
         emit TimeWindowSet(timeWindow);
+    }
+
+    /// @notice Fetches the price for a token.
+    /// @param _token The token to fetch the price for.
+    /// @return price The fetched price for the token.
+    function fetchPrice(IERC20 _token) public view returns (uint256 price) {
+        price = oracle.getRateWithCustomConnectors(_token, IERC20(stableToken), false, connectors, thresholdFilter);
     }
 
     /// @notice Fetches prices for a list of tokens.
     /// @param _tokens The tokens to fetch prices for.
     /// @return prices The fetched prices for the tokens.
-    function fetchPrices(IERC20[] calldata _tokens) public view returns (uint256[] memory prices) {
+    function fetchManyPrices(IERC20[] calldata _tokens) public view returns (uint256[] memory prices) {
         prices = oracle.getManyRatesWithCustomConnectors(_tokens, IERC20(stableToken), false, connectors, thresholdFilter);
+    }
+
+    /// @notice Records the price for a token.
+    /// @dev Only callable by owners and keepers.
+    /// @dev Emits a Price event and records it in storage.
+    /// @param _token The token to store the price for.
+    /// @param _price The price to store for the token.
+    function storePrice(address _token, uint256 _price) public onlyOwnerOrKeeper {
+        uint256 _timeWindow = timeWindow;
+        historicalPrices[_token][(block.timestamp / _timeWindow) * _timeWindow] = _price;
+        emit Price(_token, _price);
     }
 
     /// @notice Records prices for a list of tokens.
@@ -148,13 +168,13 @@ contract Prices {
     /// @dev Emits a Price event and records it in storage.
     /// @param _tokens The tokens to store prices for.
     /// @param _prices The prices to store for the tokens.
-    function storePrices(IERC20[] calldata _tokens, uint256[] calldata _prices) public {
-        _onlyOwnerOrKeeper();
+    function storeManyPrices(address[] calldata _tokens, uint256[] calldata _prices) public onlyOwnerOrKeeper {
         address token;
         uint256 price;
-        uint256 latestTimestamp = (block.timestamp / timeWindow) * timeWindow;
+        uint256 _timeWindow = timeWindow;
+        uint256 latestTimestamp = (block.timestamp / _timeWindow) * _timeWindow;
         for (uint i = 0; i < _tokens.length; i++) {
-            token = address(_tokens[i]);
+            token = _tokens[i];
             price = _prices[i];
             historicalPrices[token][latestTimestamp] = price;
             emit Price(token, price);
@@ -165,30 +185,22 @@ contract Prices {
     /// @param _token The token to return the price for.
     /// @param _timestamp The time to return the price at.
     function latest(address _token, uint256 _timestamp) public view returns (uint256) {
-        return historicalPrices[_token][((_timestamp / timeWindow) * timeWindow)];
+        uint256 _timeWindow = timeWindow;
+        return historicalPrices[_token][((_timestamp / _timeWindow) * _timeWindow)];
     }
 
     /// @notice Returns the most recent historical prices for multiple tokens based on given timestamps.
     /// @param entries Structs containing token addresses and timestamps to fetch the price for.
     /// @return prices The historial prices corresponding to the input entries.
     function latestMany(Entry[] calldata entries) public view returns (uint256[] memory) {
+        uint256 _timeWindow = timeWindow;
         uint256[] memory prices = new uint256[](entries.length);
         for (uint256 i = 0; i < entries.length; i++) {
             address token = entries[i].token;
             uint256 timestamp = entries[i].timestamp;
-            prices[i] = historicalPrices[token][((timestamp / timeWindow) * timeWindow)];
+            prices[i] = historicalPrices[token][((timestamp / _timeWindow) * _timeWindow)];
         }
 
         return prices;
-    }
-
-    /// @notice Enforces that the caller is an owner.
-    function _onlyOwner() internal view {
-        require(owners[msg.sender]);
-    }
-
-    /// @notice Enforces that the caller is an owner or a keeper.
-    function _onlyOwnerOrKeeper() internal view {
-        require(owners[msg.sender] || keepers[msg.sender]);
     }
 }
--- src/interfaces/IOffchainOracle.sol
+++ src/interfaces/IOffchainOracle.sol
@@ -5,5 +5,7 @@ pragma solidity ^0.8.13;
 import "./IERC20.sol";
 
 interface IOffchainOracle {
+    function getRateWithCustomConnectors(IERC20 srcToken, IERC20 dstToken, bool useWrappers, IERC20[] memory customConnectors, uint256 thresholdFilter) external view returns (uint256 weightedRate);
+
     function getManyRatesWithCustomConnectors(IERC20[] calldata srcTokens, IERC20 dstToken, bool useWrappers, IERC20[] calldata customConnectors, uint256 thresholdFilter) external view returns (uint256[] memory weightedRates);
 }
